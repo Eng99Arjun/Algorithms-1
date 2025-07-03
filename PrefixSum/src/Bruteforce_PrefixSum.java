03af7b29020e7f756fa56d942b35a3ccfa808813
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-07-03 23:53:33 +0530

    Add Percolation and Bruteforce_PrefixSum implementations, and configure project files

--- PrefixSum/src/Bruteforce_PrefixSum.java
+++ PrefixSum/src/Bruteforce_PrefixSum.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
 
+import java.util.Scanner;
 public class Bruteforce_PrefixSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Length : ");
+        int length = sc.nextInt();
+        int [] arr = new int[length];
+
+        for(int i = 1; i <= length; i++){
+            arr[i] =  sc.nextInt();
+        }
+
+        int [] prefixArr = new int[length];
+
+        for (int i = 0; i<length; i++){
+            int sum =0;
+            for(int j = 0; j <= i; j++){
+                sum += arr[j];
+            }
+            prefixArr[i] = sum;
+        }
+
+        System.out.println("Prefix Sum : ");
+        for(int i = 1; i <= length; i++){
+            System.out.print(prefixArr[i] + " ");
+        }
+    }
+
 }
